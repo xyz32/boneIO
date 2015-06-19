07f413bf26f1bd2334f3ca45035e5b0a5cad6910
Radu <radu32_xyz@yahoo.com>
2015-06-19 07:58:32 -0400

    rename nimble file

---
+++ examples/test.nim
@@ -0,0 +1,31 @@
+import bone, bone/gpio, os
+
+# Testing
+when isMainModule:
+  assert(bone.getPinData("P8_3").key == "P8_3")
+  try:
+    discard bone.getPinData("bla").key
+  except ValueError:
+    assert (true)
+  #end
+
+  assert ($bone.getPinData("P9_42").eeprom == "4")
+
+  try:
+    discard bone.getPinData("P9_46").gpio
+  except ValueError:
+    assert (true)
+  #end
+
+  #pinMode("P8_6", Direction.Out)
+  pinMode("P9_14", Direction.Out)
+  pinModeReset("P9_14")
+
+  pinMode("USR0", Direction.Out)
+  for i in 0..100:
+    digitalWrite("USR0", i mod 2)
+    sleep(100)
+
+  pinModeReset("USR0")
+  #end
+#end
---
+++ examples/test.nimble
@@ -0,0 +1,11 @@
+[Package]
+name          = "test"
+version       = "0.1.0"
+author        = "Anonymous"
+description   = "New Nimble project for Nim"
+license       = "BSD"
+
+bin           = "test"
+
+[Deps]
+Requires: "nim >= 0.10.0, bone"
