3ad085a9ca99669f2326964698fb6e5a213edbd8
xyz <xyz@queeq.com>
2015-07-01 21:22:50 -0400

    fix PWM file naming, and file read workaround.

--- src/bone/pwm.nim
+++ src/bone/pwm.nim
@@ -2,18 +2,31 @@
 import bone, bone/gpio, strutils
 
 const
-  slotsFile = "/sys/devices/bone_capemgr.?/slots"
+  slotsFile = "/sys/devices/bone_capemgr.9/slots"
   capeName = "am33xx_pwm"
   pwmNameTamplate = "bone_pwm_$1"
-  pwmPeriodFile = "/sys/devices/ocp.?/pwm_test_$1.??/period"
-  pwmDutyFile = "/sys/devices/ocp.?/pwm_test_$1.??/duty"
+  pwmPeriodFile = "/sys/devices/ocp.3/pwm_test_$1.15/period"
+  pwmDutyFile = "/sys/devices/ocp.3/pwm_test_$1.15/duty"
+  
+proc readFile*(fileName: string): string =
+  var tFile = open(fileName, fmRead)
+  result = ""
+  try:
+    while true:    
+      result = result & tFile.readLine() & "\n"
+    #end
+  except IOError: discard
+  finally:
+    tFile.close()
+  #end
+#end
 
 proc isCapeEnabled (): bool =
   result = contains(readFile(slotsFile), capeName)
 #end
 
 proc isPWMEnabled (pin: string): bool =
-  result = contains(readFile(slotsFile), capeName)
+  result = contains(readFile(slotsFile), pwmNameTamplate)
 #end
 
 proc pinModePWM (pin: string) =
@@ -25,7 +38,6 @@ proc pinModePWM (pin: string) =
     if not isPWMEnabled(pin):
       writeFile(slotsFile, pwmNameTamplate % [pin])
     #end
-
   else:
     raise newException(ValueError, "Pin '" & pin & "' does not support PWM")
   #end
@@ -33,7 +45,7 @@ proc pinModePWM (pin: string) =
 
 proc checkDuty (duty: float) =
   if duty < 0 or duty > 1:
-    raise newException(ValueError, "Duty is a percentage [0..1]")
+    raise newException(ValueError, "Duty is a percentage value between [0..1]. Got " & $duty)
   #end
 #end
 
@@ -46,13 +58,13 @@ proc analogWrite* (pin: string, duty: float) =
 
   checkDuty(duty)
   pinModePWM(pin)
-
+  echo readFile(pwmPeriodFile % [pin])
   var period = parseFloat(readFile(pwmPeriodFile % [pin]))
 
   writeFile(pwmDutyFile % [pin], $getDutyInNs(duty, period))
 #end
 
-proc analogWrite* (pin: string, duty: float, freqHz: int32 = 2_000) =
+proc analogWrite* (pin: string, duty: float, freqHz: int32) =
   ## Use pwm to write an analogic output.
 
   checkDuty(duty)
